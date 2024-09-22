a26d1065787af8d11121e842f02f2a3ae53528cd
Ondřej Hložek <hlozek.o1@gmail.com>
2024-09-22 12:24:04 +0200

    feat: secrets assignment

---
+++ secrets/lib/secrets.ex
@@ -0,0 +1,22 @@
+defmodule Secrets do
+  def secret_add(secret) do
+  end
+
+  def secret_subtract(secret) do
+  end
+
+  def secret_multiply(secret) do
+  end
+
+  def secret_divide(secret) do
+  end
+
+  def secret_and(secret) do
+  end
+
+  def secret_xor(secret) do
+  end
+
+  def secret_combine(secret_function1, secret_function2) do
+  end
+end
