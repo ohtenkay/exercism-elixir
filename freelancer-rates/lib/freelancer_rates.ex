29a2267eddf02da3db43e93b2869e0541b4e3b8d
Ondřej Hložek <hlozek.o1@gmail.com>
2024-09-23 13:14:56 +0200

    fix: minor improvements

--- freelancer-rates/lib/freelancer_rates.ex
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -8,19 +8,13 @@ defmodule FreelancerRates do
   end
 
   def monthly_rate(hourly_rate, discount) do
-    (22 * daily_rate(hourly_rate)) |> apply_discount(discount) |> ceil() |> trunc()
+    (22 * daily_rate(hourly_rate)) |> apply_discount(discount) |> ceil |> trunc
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
     daily_rate(hourly_rate)
     |> apply_discount(discount)
     |> (fn daily_rate -> budget / daily_rate end).()
-    |> floor(1)
-  end
-
-  def floor(number, decimal_places) do
-    (number * 10 * decimal_places)
-    |> trunc()
-    |> (fn truncated -> truncated / (10 * decimal_places) end).()
+    |> Float.floor(1)
   end
 end
