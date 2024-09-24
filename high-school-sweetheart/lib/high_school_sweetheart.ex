2bb278c18901ca834fb48e311d37953c89ebed09
Ondřej Hložek <hlozek.o1@gmail.com>
2024-09-24 17:22:32 +0200

    feat: high-school-sweetheart solution

--- high-school-sweetheart/lib/high_school_sweetheart.ex
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,36 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
-    # Please implement the first_letter/1 function
+    name |> String.trim() |> String.first()
   end
 
   def initial(name) do
-    # Please implement the initial/1 function
+    name |> first_letter() |> String.upcase() |> Kernel.<>(".")
   end
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [first_name | [last_name | _]] = full_name |> String.split()
+    initial(first_name) <> " " <> initial(last_name)
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
+    initials1 = initials(full_name1)
+    initials2 = initials(full_name2)
 
-    # Please implement the pair/2 function
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials1}  +  #{initials2}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
